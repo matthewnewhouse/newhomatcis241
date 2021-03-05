50ec6e607715de8c6eaee8a124546049884bd738
matthewnewhouse <matthewmnewhouse@gmail.com>
2021-03-05 13:54:10 -0500

    Finish Q8

---
+++ sed_project/hwinfo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+lscpu | sed -E -n '/^CPU\(s\)/p'
+echo '======='
+echo 'Memory:'
+echo '======='
+free -h | sed -n '1,2p' | sed 's/Gi/[\s]Gigabytes/g'
+echo ''
+echo 'Disks:'
+echo '======='
+df -h | sed -E -n '/[0-9]*T/p'
+echo ''
+echo 'Date:'
+echo '======='
+date | sed -E 's/Fri/Friday,/; s/Sat/Saturday,/;s/Sun/Sunday,/;s/Mon/Monday,/;s/Tue/Tuesday,/;s/Thu/Thursday,/;s/Feb/February/;s/Mar/March/;s/Apr/April/;s/Jun/June/;s/Jul/July/;s/Aug/August/;s/Sep/September/;s/Nov/November/;s/Dec/December/;s/Jan/January/;s/AM/a\.m\./;s/PM/p\.m\./;s/EST/Eastern Standard/;s/CST/Central Standard/;s/PST/Pacific Standard/;s/MST/Mountain Standard/'| sed -E 's/ /\./4'
