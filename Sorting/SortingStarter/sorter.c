3f9ab9c562aabccfd4baa59b4bf4d3927f4f1da0
matthewnewhouse <matthewmnewhouse@gmail.com>
2021-04-10 18:15:38 -0400

    Save Comments

--- Sorting/SortingStarter/sorter.c
+++ Sorting/SortingStarter/sorter.c
@@ -4,7 +4,8 @@
 
 void sort(char** contents, int size){
 
-	
+
+	//Make Array Of Strings From Contents	
 	char** array= (char**)malloc(sizeof(char*)*size);
 	
 	int i = 0;
@@ -21,16 +22,7 @@ void sort(char** contents, int size){
 	         r = strtok(NULL, "\n");
 	}
 
-	/*	
-	prntf("\nARRAY AFTER LOAD:%s\n","");
-	for (int w = 0; w<size; w++){
-		for(int u = 0; u<strlen(array[w]); u++){
-
-			printf("%c", (array)[w][u]);
-		}
-	}
-	*/
-
+	//Sort Array
 	 int m;
 	 char* key;
 	 int n=0;
@@ -44,72 +36,21 @@ void sort(char** contents, int size){
 		}
 		array[n+1] = key;
 	}
-
-	/*
-	printf("\nARRAY AFTER SORT:%s\n","");
-	for (int w = 0; w<size; w++){
-		for(int u = 0; u<2; u++){
-
-		printf("%c", (array)[w][u]);
-	}
-	}
-
-	printf("\nCONTENTS BEFORE COPY:%s\n","");
-	for (int w = 0; w<1; w++){
-		for(int u = 0; u<15; u++){
-
-			printf("%c", (contents)[0][u]);
-		}
-	}
-	*/
-
-	/*
-	contents[0][3] = 'g';
-
-	printf("CONTENTS[0]:%s\n",contents[0]);
-	printf("CONTENTS[0][0]:%c\n",contents[0][3]);
-
-	*/
 		
-
-	 int z = 0;
+	
+	//Copy Array To Contents
+	int z = 0;
 	for(int x = 0; x<size; x++){
 
 		for(int y = 0; y<strlen(array[x]) ; y++){
-		/*
-			printf("\nARRAY IN COPY:%s\n","");
-			for (int w = 0; w<size; w++){
-			for(int u = 0; u<2; u++){
-
-
-			printf("%c", (array)[w][u]);}}
-			printf("%s", "\n");
 	
-
-			printf("\nCONTENTS IN COPY:%s\n","");
-
-			for (int w = 0; w<1; w++){
-	
-				for(int u = 0; u<z; u++){
-
-		printf("%c", (array)[0][u]);}}
-			printf("CONTENTS MATRIX BEFORE:%c\n", contents[0][z]);
-			printf("CHANGE TO:%c\n", array[x][y]);
-			printf("x=%d\n", x);
-			printf("y=%d\n", y);
-			*/
 			contents[0][z] = array[x][y];
-			//printf("CONTENTS MATRIX AFTER:%c\n", contents[0][z]);
 			z++;
 		}
-		//printf("CONTENTS MATRIX BEFORE:%c\n", contents[0][z]);
 		contents[0][z] = '\n';
-		//printf("CONTENTS MATRIX AFTER:%c\n", contents[0][z]);
 		z++;
-		//printf("\nEND OF LOOP:%d\n", x);
 	}
 
-	//printf("BYTES:%d\n", z);
-
+	//Free Memory
 	free(array);
 }
