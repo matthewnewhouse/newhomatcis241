1b610fe06537646fe9732135f0f55f3de45d4367
matthewnewhouse <78094787+matthewnewhouse@users.noreply.github.com>
2022-02-15 20:08:11 -0500

    Update ddl.sql

--- cis343/ddl.sql
+++ cis343/ddl.sql
@@ -19,7 +19,7 @@ cost INTEGER NOT NULL,
 /*
 IC1: The priority is one of: high, medium, or low
 */
-CONSTRAINT IC1 CHECK (priority = "high" OR priority = "medium" or priority = "low),
+CONSTRAINT IC1 CHECK (priority = "high" OR priority = "medium" OR priority = "low"),
 /*
 IC2: The cost of a high priority order is above 2000.
 */
@@ -33,6 +33,47 @@ IC4: The cost of a low priority order is less than 1000.
 */
 CONSTRAINT IC4 CHECK (NOT (priority = "low" AND cost > 1000)),
 );
+--
+--
+CREATE TABLE OrderLine
+(
+orderNum INTEGER,
+lineNum INTEGER,
+item CHAR (10) NOT NULL,
+quantity INTEGER,
+PRIMARY KEY (orderNum, lineNum),
+/*
+IC5: Every order line must belong to an order in the Order table.
+Also: if an order is deleted then all its order lines must be deleted.
+IMPORTANT: DO NOT declare this IC as DEFERRABLE.
+*/
+CONSTRAINT IC5 FOREIGN KEY (orderNum) REFERENCES Orders(orderNum) ON DELETE CASCADE
+);
+--
+-- ----------------------------------------------------------------
+-- TESTING THE SCHEMA
+-- ----------------------------------------------------------------
+INSERT INTO Orders VALUES (10, 'high', 2400);
+INSERT INTO Orders VALUES (20, 'high', 1900);
+INSERT INTO Orders VALUES (30, 'high', 2100);
+INSERT INTO Orders VALUES (40, 'medium', 700);
+PA-05-DDLc-OL.doc Page 3 of 3
+INSERT INTO Orders VALUES (50, 'low', 1100);
+INSERT INTO Orders VALUES (60, 'low', 900);
+SELECT * from Orders;
+-- ----------------------------------------------------------------
+INSERT INTO OrderLine VALUES (10, 1, 'AAA', 200);
+INSERT INTO OrderLine VALUES (10, 2, 'BBB', 300);
+INSERT INTO OrderLine VALUES (60, 1, 'CCC', 5);
+INSERT INTO OrderLine VALUES (15, 1, 'AAA', 7);
+SELECT * FROM OrderLine;
+--
+DELETE FROM Orders WHERE orderNum = 10;
+SELECT * From Orders;
+SELECT * FROM OrderLine;
+--
+SET ECHO OFF
+SPOOL OFF
 --
 --
 CREATE TABLE OrderLine
