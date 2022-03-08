b69cd60e40dd6963beb3db9cd18d3830552f5d83
matthewnewhouse <78094787+matthewnewhouse@users.noreply.github.com>
2022-03-07 19:14:13 -0500

    Update StudentBoard.cpp

--- battleshipAssignment/StudentBoard.cpp
+++ battleshipAssignment/StudentBoard.cpp
@@ -20,9 +20,17 @@ void Board::setVisible(bool v){
 }
 
 int& Board::Internal::operator[](int index){
+        if(index >= WIDTH){
+                throw std::out_of_range(std::to_string(index) + " is greater value than or equal to grid width of " + std::to_string(WIDTH));
+        }
+        return _grid[index];
 }
 
 Board::Internal Board::operator[](int index){
+        if(index >= HEIGHT){
+                throw std::out_of_range(std::to_string(index) + " is greater value than or equal to grid height of " + std::to_string(HEIGHT));
+        }
+        return Board::Internal(grid+(index * WIDTH));
 }
 
 std::ostream& operator<<(std::ostream& os, Board const& b){
