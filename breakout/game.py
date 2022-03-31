03798afb8c6aeb0aa4e344660e338e57f20cb992
matthewnewhouse <matthewmnewhouse@gmail.com>
2022-03-30 21:59:25 -0400

    Add More Elements

--- breakout/game.py
+++ breakout/game.py
@@ -6,18 +6,28 @@ class Game:
     def __init__(self):
         pg.init()
         self.__running = False
-        screenWidth = 800
-        screenHeight = 600
-        self.screen = pg.display.set_mode((screenWidth,screenHeight))
+        self.screenWidth = 800
+        self.screenHeight = 600
+        self.screen = pg.display.set_mode((self.screenWidth,self.screenHeight))
         pg.display.set_caption("Breakout")
         self.clock = pg.time.Clock()
         self.spriteList = pg.sprite.Group()
 
-        self.paddle = Paddle(100)
-        self.paddle.rect.x = screenWidth/2 - self.paddle.w/2
-        self.paddle.rect.y = screenHeight-50
+        self.overlay = Overlay(3)
+        self.overlay.rect.x = 0
+        self.overlay.rect.y = 0
 
+        self.paddle = Paddle(100)
+        self.paddle.rect.x = self.screenWidth/2 - self.paddle.w/2
+        self.paddle.rect.y = self.screenHeight-50
+
+        self.ball = Ball()
+        self.ball.rect.x = self.screenWidth/2 - self.ball.w/2
+        self.ball.rect.y = self.screenHeight/2 - self.ball.h/2
+
+        self.spriteList.add(self.overlay)
         self.spriteList.add(self.paddle)
+        self.spriteList.add(self.ball)
     def run(self):
         while self.__running:
             events = pg.event.get()
@@ -34,6 +44,21 @@ class Game:
             
             self.spriteList.update()
 
+            if self.ball.rect.x >= self.screenWidth:
+                self.ball.velocityX*=-1
+            elif self.ball.rect.x<=0:
+                self.ball.velocityX*=-1
+            if self.ball.rect.y >= self.screenHeight:
+                self.ball.velocityY*=-1
+            elif self.ball.rect.y<=0:
+                self.ball.velocityY*=-1
+
+        
+            if pg.sprite.collide_mask(self.ball,self.paddle):
+                self.ball.rect.x -= self.ball.velocityX
+                self.ball.rect.y -= self.ball.velocityY
+                self.ball.bounce()
+
             self.screen.fill((255,255,255))
             self.spriteList.draw(self.screen)
 
@@ -41,6 +66,18 @@ class Game:
             self.clock.tick(60)
     def setRunning(self, running):
         self.__running = running
+
+class Overlay(pg.sprite.Sprite):
+    def __init__(self,lives):
+        super().__init__()
+        self.lives = lives
+        self.score = 0
+
+        font = pg.font.Font(None,24)
+        self.image = font.render("Lives: " + str(self.lives) + "   Score: " + 
+                str(self.score), 1, (50,50,50))
+        self.rect = self.image.get_rect()
+        pg.draw.rect(self.image,0,self.rect,1)
 
 class Paddle(pg.sprite.Sprite):
     def __init__(self,width):
@@ -66,6 +103,30 @@ class Paddle(pg.sprite.Sprite):
         if self.rect.x > (800-self.w):
             self.rect.x = 800-self.w
 
+class Ball(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.w = 15
+        self.h = 15
+
+        self.image = pg.Surface([self.w,self.h])
+        self.image.fill((50,50,200))
+
+        pg.draw.rect(self.image, (50,50,200), [0,0,self.w,self.h])
+        self.velocityX = 3
+        self.velocityY = 3
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.x += self.velocityX
+        self.rect.y += self.velocityY
+
+    def bounce(self):
+        #self.velocityX*=-1
+        self.velocityY*=-1
+        
 def main():
     game = Game()
 
