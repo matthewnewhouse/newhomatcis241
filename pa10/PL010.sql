74abb20a3057857c8cdee2c8bb89ccd8e0d61e8d
matthewnewhouse <78094787+matthewnewhouse@users.noreply.github.com>
2022-04-07 21:40:19 -0400

    Add files via upload

---
+++ pa10/PL010.sql
@@ -0,0 +1,20 @@
+-- PL010
+-- author: JRA
+-- -----------------------------------------------------------
+-- display result of procedure DBMS_OTPUT.PUT_LINE
+SET SERVEROUTPUT ON 
+-- Don't display the before/after caused by substitution variable, s
+SET VERIFY OFF
+
+-- declaring a bind variable, b, in SQL*plus
+VARIABLE b NUMBER;
+
+-- Entering a PL/SQL block - notice how we reference s and b.
+BEGIN
+   :b := &&s + 1;
+   DBMS_OUTPUT.PUT_LINE('+++++ here it is .. '||:b||' and '|| &s);
+END;
+/
+-- Now printing, in SQL*Plus, the bind variable b.
+PRINT b;
+UNDEFINE s   //just to clean up the environment
