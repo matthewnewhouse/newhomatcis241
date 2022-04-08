891ca5c18d8717b1d94a1f33939a233be1fa577d
matthewnewhouse <matthewmnewhouse@gmail.com>
2022-04-08 11:03:56 -0400

    API SETUP

---
+++ api/index.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const csv = require('csv-parse')
+
+let heroes = {};
+
+const parser = csv.parse(
+	{ columns: true }, 
+	function(err, records){
+		heroes = records;
+	}
+);
+
+fs.createReadStream(__dirname + '/superheroes.csv').pipe(parser)
+
+const express = require('express');
+const app = express()
+const port = 3000;
+
+app.use(express.static('public'))
+
+app.get('/', 
+	(req, res) => {
+		res.send('<h1>Welcome to our awesome Superhero API!</h1>');
+	}
+);
+
+app.get('/heroes',
+	(req, res) => {
+		res.send(heroes);
+	}
+);
+
+app.get('/search/:term',
+	(req, res) => {
+		let selected = [];
+		heroes.forEach(
+			e => {
+				if(e.Name.includes(req.params.term)){
+					selected.push(e);
+				}
+			}
+		)
+		res.send(selected);
+	}
+);
+
+app.listen(port, () => {
+	console.log("I've become aware.");
+});
